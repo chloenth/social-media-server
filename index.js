dc206e3f00eac3023d9c9ebb4828948896f035fa
Huong <huongnguyen.nth1709@gmail.com>
2024-04-22 11:21:57 +0300

    remove unnecessary code

--- index.js
+++ index.js
@@ -15,10 +15,6 @@ import userRoutes from './routes/users.js';
 import postRoutes from './routes/posts.js';
 import { verifyToken } from './middleware/auth.js';
 
-import User from './models/User.js';
-import Post from './models/Post.js';
-import { users, posts } from './data/index.js';
-
 // CONFIGURATIONS
 
 const __filename = fileURLToPath(import.meta.url);
